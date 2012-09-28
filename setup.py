8b6db65e2998d584a0c586bd5da1eb15694d0478
Michael Coffey <michaelc@last.fm>
2012-09-28 15:45:24 +0100

    Update the status in Messages and Adium to your last Twitter status.

---
+++ setup.py
@@ -0,0 +1,51 @@
+import os
+import sys
+
+plist = 'uk.co.mobbler.twims.plist'
+
+def install(username):
+	string = ""
+
+	# read from the plist
+	with open(os.path.realpath(plist), 'r') as f:
+		string = f.read()
+
+	# replace the plist strings
+	string = string.replace("%PYTHON%", os.popen('which python').read().strip())
+	string = string.replace("%SCRIPT%", os.path.realpath('twims.py'))
+	string = string.replace("%USERNAME%", username)
+
+	if os.path.isfile(os.path.expanduser('~/Library/LaunchAgents/' + plist)):
+		os.popen('launchctl unload -w ' + os.path.expanduser('~/Library/LaunchAgents/' + plist))
+
+	# write the plist
+	with open(os.path.expanduser('~/Library/LaunchAgents/' + plist), 'w') as f:
+		string = f.write(string)
+
+	# don't wait for a restart for launchd to notice this
+	os.popen('launchctl load -w ' + os.path.expanduser('~/Library/LaunchAgents/' + plist))
+
+	# run the script now so the user sees an immediate difference
+	os.system( 'python twims.py ' + username)
+
+def uninstall():
+	if os.path.isfile(os.path.expanduser('~/Library/LaunchAgents/' + plist)):
+		os.popen('launchctl unload -w ' + os.path.expanduser('~/Library/LaunchAgents/' + plist))
+		os.popen('rm ' + os.path.expanduser('~/Library/LaunchAgents/' + plist))
+
+def usage():
+	sys.exit('Usage:\nInstall: setup.py install <username>\nUninstall: setup.py rm\n')
+
+# change the working directory to the script location 
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+# very basic argument parsing
+if sys.argv[1] == "install":
+	if len(sys.argv) < 3:
+		usage()
+	else:
+		install(sys.argv[2])
+elif sys.argv[1] == "rm":
+	uninstall()
+else:
+	usage()
---
+++ twims.py
@@ -0,0 +1,52 @@
+import sys
+import xml.etree.ElementTree
+import urllib2
+import os
+
+def run_applescript(script):
+	cmd = "osascript -e '" + script.replace("'","'\\''") + "'"
+	return os.popen(cmd).read().strip()
+
+def is_app_running(app):
+	return run_applescript('tell application "System Events" to (name of processes) contains "' + app + '"') == "true"
+
+def update_app_status(app, status):
+	if is_app_running(app):
+		run_applescript('tell application "' + app + '" to set status message to "' + status + '"')
+
+def add_param(url, key, value):
+	return url + key + "=" + value + "&"
+
+# fetch the latest tweet that wasn't a reply, including RTs
+url = 'http://api.twitter.com/1/statuses/user_timeline.xml?'
+url = add_param(url, 'screen_name', sys.argv[1])
+url = add_param(url, 'trim_user', '1')
+url = add_param(url, 'include_rts', '1')
+url = add_param(url, 'count', '1')
+url = add_param(url, 'exclude_replies', '1')
+url = add_param(url, 'include_entities', '1')
+
+response = urllib2.urlopen(url)
+html = response.read()
+
+# parse the responce and get the status
+statuses = xml.etree.ElementTree.XML(html)
+status = statuses[0].find('text').text
+
+# replace the url entities with the expanded urls
+offset = 0
+
+for url in statuses[0].find("entities").find("urls"):
+	start = int(url.attrib['start']) + offset
+	end = int(url.attrib['end']) + offset
+
+	# take away the length of the original string, add the length of the replacment string
+	offset = offset - (end - start) + len(url.find('expanded_url').text)
+
+	status = status[:start] + url.find('expanded_url').text + status[end:]
+
+print status
+
+# Could add others here. Maybe it should be configurable 
+update_app_status("Messages", status)
+update_app_status("Adium", status)
